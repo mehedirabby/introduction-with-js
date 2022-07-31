edf5ef645f3cb7a6671e54d4f924fdbd32d8fd87
unknown <mehedi15-2995@diu.edu.bd>
2022-07-31 07:14:41 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,16 @@
+/***
+ * 1.Number ==> 55.898,-32
+ * 2.integer,odd,even: 2,5
+ * 3.float: decimal: 2.5,13.67,32.69
+ * 4.operations: addition,substraction,multiplication,devision,reminder
+ * 5.percent
+ * 6.avarage
+ * 7.comparison, >,<,==,!=,>=,=<
+ * 8.oikik niyom. 
+ * 9.45,25,65,129,2;max,min,avrg,sum
+ * 10. series:
+ * ::::EXTRA:::
+ * 1.Prime
+ * 2.permutation
+ * 3.area
+ * */
---
+++ variable.js
@@ -0,0 +1,17 @@
+// vary + able = variable
+var price = 13;
+var age = 17;
+var temparature = 38;
+var weight = 72;
+var networth = 20000000;
+
+
+// string
+var person = "sodor uddin";
+var location = "board bazar";
+var special = 'alia bhatt';
+
+// boolean
+ var serious = true;
+ var isFullNarks = true;
+ var isSingle = false;
