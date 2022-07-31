f3c74f4465d8683954bcb3ff5c5da87fb01a151d
unknown <mehedi15-2995@diu.edu.bd>
2022-07-31 09:59:59 +0600

    another-commit

---
+++ operations.js
@@ -0,0 +1,24 @@
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice + eggPrice;
+//addition and substraction
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference);
+
+console.log(totalPrice);
+//console.log(onionPrice);
+//console.log(eggPrice);
+//console.log(onionPrice + eggPrice);
+
+//multiplication
+var orangePrice = 20;
+var quantity = 7;
+var totalCost = orangePrice * quantity;
+console.log(totalCost);
+
+//devision
+
+var money = 500;
+var player = 10;
+var eacPlayer = money / player ;
+console.log(eacPlayer);
---
+++ shorthand.js
@@ -0,0 +1,44 @@
+//variable declaration
+var tomatoPrice = 25;
+//variable update
+tomatoPrice = 30;
+
+
+
+// add something to the previous value
+var price1 = 31;
+var price2 = 10;
+
+
+//set a new value
+//price1 = 35;
+
+//add some value to the existing value
+price1 = price1 + 10;
+console.log(price1)
+
+// addition to the same variable shortHand
+//=+ means adding some value with existing value
+price1 += 10;
+
+
+price2 = price2 - 5;
+//shorthand -=
+//=- means deducting some values from the current value of the variable
+price2 -= 5;
+// *=
+price1 *= 5;
+// /=
+
+console.log(price1);
+var age = 15;
+age = age+1;
+//++ means adding 1 to the existing value
+ age ++
+
+
+ var love = 100;
+ love = love - 1;
+ // -- means deducting 1 from existing value
+ love --;
+
