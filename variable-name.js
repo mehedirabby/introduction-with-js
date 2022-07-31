9e989ecb404b95a7a21c90d7d2124e9007c93bd0
unknown <mehedi15-2995@diu.edu.bd>
2022-07-31 07:34:10 +0600

    variable declared

---
+++ variable-name.js
@@ -0,0 +1,25 @@
+var price = 29;
+
+
+// 1. variable name cannot be any keyWords.
+var false = 96 ;
+var return = true;
+
+// 2.variable name has to be in one word.
+var my home address = "new california";
+
+// 3.variable name can't have cotation
+var "name" = "rabby hasan";
+
+//4.variable name can't start with a number
+var 99club = 1964;
+var club99 = 2022;
+// 5.how to use long name
+// can't use dash
+var user-name = "raju vai";
+var user_name = "bappa raz";
+var usercurrentaddress = "wrong war";
+var user_home_address = "wrong way"; //snake case
+var user home adddress = "error ashbe";
+var userHomeAddress = "right way";  // camel case
+var UserHomeAddress = "sometimes rght" //pascel case
